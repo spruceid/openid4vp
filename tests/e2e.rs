517706d64ece18bc148478094ce9969d54dfd214
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2024-10-15 11:58:32 -0400

    Improve `VpToken` type & other type improvements. (#31)

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -134,7 +134,7 @@ async fn w3c_vc_did_client_direct_post() {
 
     let response = AuthorizationResponse::Unencoded(UnencodedAuthorizationResponse(
         Default::default(),
-        vp.try_into().expect("failed to convert vp to vp token"),
+        vp.into(),
         presentation_submission.try_into().unwrap(),
     ));
 
