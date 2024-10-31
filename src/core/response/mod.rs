cf66d9380b751d099889a8c1a5ace618ef507058
Ryan Tate <ryan.tate@spruceid.com>
2024-10-31 12:56:39 -0700

    update url encoding for auth response
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- src/core/response/mod.rs
+++ src/core/response/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use super::{object::UntypedObject, presentation_submission::PresentationSubmission};
 
 use anyhow::{Context, Error, Result};
@@ -36,8 +38,20 @@ pub struct UnencodedAuthorizationResponse {
 impl UnencodedAuthorizationResponse {
     /// Encode the Authorization Response as 'application/x-www-form-urlencoded'.
     pub fn into_x_www_form_urlencoded(self) -> Result<String> {
-        serde_urlencoded::to_string(&self)
-            .context("failed to encode response as 'application/x-www-form-urlencoded'")
+        let mut map = HashMap::<&str, String>::new();
+        let vp_token =
+            serde_json::to_string(&self.vp_token).context("failed to parse JSON vp token")?;
+        let presentation_submission = serde_json::to_string(&self.presentation_submission)
+            .context("failed to encode presentation_submission as JSON")?;
+
+        map.insert("presentation_submission", presentation_submission);
+        map.insert("vp_token", vp_token);
+
+        let encoded = serde_urlencoded::to_string(&map).context(
+            "failed to encode presentation_submission as 'application/x-www-form-urlencoded'",
+        )?;
+
+        Ok(encoded)
     }
 
     /// Return the Verifiable Presentation Token.
@@ -113,7 +127,7 @@ mod test {
         let response = UnencodedAuthorizationResponse::try_from(object).unwrap();
         assert_eq!(
             response.into_x_www_form_urlencoded().unwrap(),
-            "presentation_submission=%7B%22id%22%3A%22d05a7f51-ac09-43af-8864-e00f0175f2c7%22%2C%22definition_id%22%3A%22f619e64a-8f80-4b71-8373-30cf07b1e4f2%22%2C%22descriptor_map%22%3A%5B%5D%7D&vp_token=string",
+            "presentation_submission=%7B%22id%22%3A%22d05a7f51-ac09-43af-8864-e00f0175f2c7%22%2C%22definition_id%22%3A%22f619e64a-8f80-4b71-8373-30cf07b1e4f2%22%2C%22descriptor_map%22%3A%5B%5D%7D&vp_token=%22string%22",
         )
     }
 }
